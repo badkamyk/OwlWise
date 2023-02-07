9f05f2da640e922b17a437f131d8926dbdeab9b8
badkamyk <slothmateusz@gmail.com>
2023-02-07 11:19:15 +0100

    feat: added heading atoms and molecule

---
+++ src/components/atoms/HeadingMain/index.tsx
@@ -0,0 +1,5 @@
+export default function MainHeading() {
+    return (
+        <h1 className="mb-3 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Better Data</span> Scalable AI.</h1>
+    )
+}
