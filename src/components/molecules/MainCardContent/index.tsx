4994d9e939318bd877e2deeb45658aee3be4a63c
badkamyk <slothmateusz@gmail.com>
2023-02-07 19:56:04 +0100

    feat: added animation to main page cards

--- src/components/molecules/MainCardContent/index.tsx
+++ src/components/molecules/MainCardContent/index.tsx
@@ -1,16 +1,30 @@
 import MainCardDescription from '../../atoms/MainCardDescription'
 import MainCardImg from '../../atoms/MainCardImg'
 import MainCardTitle from '../../atoms/MainCardTitle'
+import { useInView } from "react-intersection-observer";
+import { useSpring, animated } from "@react-spring/web";
 import { MainCardType } from './MainCardType'
 
 export default function MainCardContent({ title, description, src }: MainCardType) {
+    const { ref, inView } = useInView({
+        threshold: 0.5,
+    });
+
+    const animation = useSpring({
+        opacity: inView ? 1 : 0,
+        transform: inView ? "translateY(0)" : "translateY(50px)",
+        config: {
+            duration: 1000,
+        },
+    });
+
     return (
-        <div className="relative rounded-[15px] bg-white p-6">
+        <animated.div className="relative rounded-[15px] bg-white p-6" ref={ref} style={animation}>
             <div className="space-y-4">
                 <MainCardImg src={src} alt="Image with particles" />
                 <MainCardTitle text={title} />
                 <MainCardDescription text={description} />
             </div>
-        </div>
+        </animated.div>
     )
 }
