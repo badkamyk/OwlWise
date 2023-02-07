d5ef80c1210489d3e2528a202abf543906a93b77
badkamyk <slothmateusz@gmail.com>
2023-02-07 12:30:53 +0100

    feat: added heading animation

--- src/components/molecules/HeadingText/index.tsx
+++ src/components/molecules/HeadingText/index.tsx
@@ -1,11 +1,33 @@
 import HeadingParagraph from "../../atoms/HeadingParagraph";
 import MainHeading from "../../atoms/HeadingMain";
+import { useTrail, animated } from "@react-spring/web";
+import React from "react";
+
+const Trails = ({ children }: { children: React.ReactNode }) => {
+    const items = React.Children.toArray(children);
+    const trail = useTrail(items.length, {
+        from: { opacity: 0, transform: "translate3d(0,40px,0)" },
+        to: { opacity: 1, transform: "translate3d(0,0px,0)" },
+        config: { duration: 1200 }
+    });
+    return (
+        <div className="p-3 text-center">
+            {trail.map(({ ...style }, index) => (
+                <animated.div key={index} style={style}>
+                    {items[index]}
+                </animated.div>
+            ))}
+        </div>
+    );
+};
 
 export default function HeadingText() {
     return (
-        <div className="p-3 text-center">
+        <Trails>
             <MainHeading />
             <HeadingParagraph />
-        </div>
+        </Trails>
+
     )
 }
+
--- src/components/organisms/Heading/index.tsx
+++ src/components/organisms/Heading/index.tsx
@@ -1,9 +1,19 @@
 import HeadingText from "../../molecules/HeadingText";
+import { useSpring, animated } from '@react-spring/web'
+
 
 export default function Heading() {
+    const [props, api] = useSpring(
+        () => ({
+            from: { opacity: 0 },
+            to: { opacity: 1 },
+            config: { duration: 2100 }
+        }),
+        []
+    )
     return (
-        <div className="bg-[url('/public/ales-nesetril-Im7lZjxeLhg-unsplash.webp')] bg-cover bg-center bg-no-repeat bg-fixed h-[50vh]">
+        <animated.div style={props} className="bg-[url('/public/ales-nesetril-Im7lZjxeLhg-unsplash.webp')] bg-cover bg-center bg-no-repeat bg-fixed h-[50vh]">
             <HeadingText />
-        </div>
+        </animated.div>
     )
 }
