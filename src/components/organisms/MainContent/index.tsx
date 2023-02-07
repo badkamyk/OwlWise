a8b164a58966cd38667d232adb0895ad8ea5152b
badkamyk <slothmateusz@gmail.com>
2023-02-07 21:53:18 +0100

    feat: added content section with basic animation

---
+++ src/components/organisms/MainContent/index.tsx
@@ -0,0 +1,50 @@
+import MainContentText from "../../molecules/MainContentText";
+import MainContentImages from "../../molecules/MainContentImages";
+import { useInView } from "react-intersection-observer";
+import { useTrail, animated } from "@react-spring/web";
+
+const textInfo = {
+    headingText: "We didn't reinvent the wheel",
+    paragraphText: "We are strategists, designers and developers. Innovators and problem solvers. Small enough " +
+        "to be simple and quick, but big enough to deliver the scope you want at the pace you need. " +
+        "Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.",
+    secondaryText: "We act as an extension of your team, working with you to deliver the best possible product.",
+    headingClassName: "mb-4 text-4xl md: text-5xl tracking-tight font-extrabold text-gray-900",
+    paragraphClassName: "mb-4 text-gray-900 text-md md:text-2xl",
+}
+
+export default function MainContent() {
+    const { ref, inView } = useInView({
+        threshold: 0.5,
+        triggerOnce: true,
+    });
+
+    const trail = useTrail(2, {
+        opacity: inView ? 1 : 0,
+        transform: inView ? "translate3d(0,0px,0)" : "translate3d(0,-40px,0)",
+        config: { duration: 900 }
+    });
+
+    return (
+        <section
+            className="py-12 bg-gradient-to-b text-center from-teal-100 via-rose-100 to-gray-500 divide-y-8">
+            <div
+                className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6"
+                ref={ref}>
+                {trail.map(({ ...style }, index) => (
+                    <animated.div key={index + "mainContent"} style={style}>
+                        {index === 0 ? <MainContentText
+                                headingText={textInfo.headingText} paragraphText={textInfo.paragraphText}
+                                secondaryText={textInfo.secondaryText} headingClassName={textInfo.headingClassName}
+                                paragraphClassName={textInfo.paragraphClassName}/>
+                            : <MainContentImages
+                                firstSrc={"https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"}
+                                firstAlt={"People in the office"}
+                                secondSrc={"https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"}
+                                secondAlt={"Office equipment"}/>}
+                    </animated.div>
+                ))}
+            </div>
+        </section>
+    )
+}
