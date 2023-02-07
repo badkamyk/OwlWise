ff157e6aea4f749699163e121def63649ef1b32a
badkamyk <slothmateusz@gmail.com>
2023-02-07 11:47:07 +0100

    refactor: changed img to webp format

--- src/components/organisms/Heading/index.tsx
+++ src/components/organisms/Heading/index.tsx
@@ -2,7 +2,7 @@ import HeadingText from "../../molecules/HeadingText";
 
 export default function Heading() {
     return (
-        <div className="bg-[url('/public/ales-nesetril-Im7lZjxeLhg-unsplash.jpg')] bg-cover bg-center bg-no-repeat bg-fixed h-[50vh]">
+        <div className="bg-[url('/public/ales-nesetril-Im7lZjxeLhg-unsplash.webp')] bg-cover bg-center bg-no-repeat bg-fixed h-[50vh]">
             <HeadingText />
         </div>
     )
