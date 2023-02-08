c683b4036aa2640c72d47ab683847956991001e4
badkamyk <slothmateusz@gmail.com>
2023-02-08 21:44:56 +0100

    feat: added ErrorPage

--- src/components/organisms/ErrorPage/index.tsx
+++ src/components/organisms/ErrorPage/index.tsx
@@ -1,20 +1,32 @@
+import HeadingButton from "../../atoms/HeadingButton";
 import ContentHeading from "../../atoms/MainContentHeading";
 import MainContentParagraph from "../../atoms/MainContentParagraph";
-import HeadingButton from "../../atoms/HeadingButton";
 
 export default function ErrorPage() {
 	return (
-		<section className="bg-gray-900 min-h-screen">
-			<div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+		<section className="min-h-screen bg-gray-900">
+			<div className="mx-auto max-w-screen-xl py-8 px-4 lg:py-16 lg:px-6">
 				<div className="mx-auto max-w-screen-sm text-center">
 					<ContentHeading
-						className={"mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-white"}
-						text={"404"} />
+						className={
+							"mb-4 text-7xl font-extrabold tracking-tight text-white lg:text-9xl"
+						}
+						text={"404"}
+					/>
 					<MainContentParagraph
-						className={"mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white"}
-						text={"Something's missing."} />
-					<MainContentParagraph className={"mb-4 text-lg font-light text-gray-500 dark:text-gray-400"}
-																text={"Sorry, we can't find that page. You'll find lots to explore on the home page."} />
+						className={
+							"mb-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white md:text-4xl"
+						}
+						text={"Something's missing."}
+					/>
+					<MainContentParagraph
+						className={
+							"mb-4 text-lg font-light text-gray-500 dark:text-gray-400"
+						}
+						text={
+							"Sorry, we can't find that page. You'll find lots to explore on the home page."
+						}
+					/>
 					<HeadingButton text={"Back to Homepage"} href={"/"} />
 				</div>
 			</div>
