bdc7e773fe4b749b29df14d083ddb72b9af2477f
badkamyk <slothmateusz@gmail.com>
2023-02-09 13:49:48 +0100

    style: changed footer and nav link behaviour

--- src/components/atoms/FooterLink/index.tsx
+++ src/components/atoms/FooterLink/index.tsx
@@ -1,7 +1,9 @@
 export default function FooterLink({ href }: { href: string }) {
+	const isHome = href === "Home";
+
 	return (
 		<a
-			href={`${href.toLowerCase()}`}
+			href={isHome ? "/" : href}
 			className="text-base leading-6 text-gray-100 hover:text-gray-400"
 		>
 			{href}
--- src/components/atoms/NavLink/index.tsx
+++ src/components/atoms/NavLink/index.tsx
@@ -2,7 +2,7 @@ export default function NavLink({ href }: { href: string }) {
 	return (
 		<li>
 			<a
-				href={`/${href.toLowerCase()}`}
+				href={href === "Home" ? "/" : `/${href.toLowerCase()}`}
 				className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-400"
 				aria-current="page"
 			>
