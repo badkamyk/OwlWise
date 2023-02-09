4a2b0a046504e7b862e169d837f88463caa1a97d
badkamyk <slothmateusz@gmail.com>
2023-02-09 13:38:29 +0100

    refactor: changed FooterMediaLinkType, added AboutCardType

--- src/components/atoms/SocialMediaLink/FooterMediaLinkType.tsx
+++ src/components/atoms/SocialMediaLink/FooterMediaLinkType.tsx
@@ -1,4 +1,4 @@
-export type FooterMediaLinkType = {
+export type SocialMediaLinkType = {
 	href: string;
 	srText: string;
 	path: JSX.Element;
---
+++ src/components/molecules/AboutCard/AboutCardType.tsx
@@ -0,0 +1,9 @@
+export type AboutCardType = {
+	name: string;
+	description: string;
+	fbName: string;
+	twName: string;
+	ghName: string;
+	role: string;
+	imgSrc: string;
+}
