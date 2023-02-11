047d4c9d093265f761a1e94cc11214a96a8bb6fa
badkamyk <slothmateusz@gmail.com>
2023-02-11 12:27:20 +0100

    refactor: changed CompleteInput molecule to be using forwardRef and hook-form

--- src/components/molecules/CompleteInput/index.tsx
+++ src/components/molecules/CompleteInput/index.tsx
@@ -1,18 +1,22 @@
 import Input from "../../atoms/Input";
 import InputLabel from "../../atoms/InputLabel";
 import { CompleteInputType } from "./CompleteInputType";
+import { useFormContext } from "react-hook-form";
 
-export default function CompleteInput({
+const CompleteInput = ({
 	text,
 	type,
 	id,
 	placeholder,
 	required,
-}: CompleteInputType) {
+}: CompleteInputType) => {
+	const { register } = useFormContext();
 	return (
 		<div>
 			<InputLabel text={text} htmlFor={id} />
-			<Input {...{ type, id, placeholder, required }} />
+			<Input {...{ type, id, placeholder, required }} {...register(id)} />
 		</div>
 	);
-}
+};
+
+export default CompleteInput;
