69b76b67b3b1f06f02d26834a14fc474973461b4
badkamyk <slothmateusz@gmail.com>
2023-02-11 12:26:51 +0100

    refactor: changed TextArea atom to be using forwardRef and hook-form

--- src/components/atoms/TextArea/index.tsx
+++ src/components/atoms/TextArea/index.tsx
@@ -1,5 +1,6 @@
 import InputLabel from "../InputLabel";
 import { TextAreaType } from "./TextAreaType";
+import { useFormContext } from "react-hook-form";
 
 export default function TextArea({
 	id,
@@ -7,6 +8,7 @@ export default function TextArea({
 	placeholder,
 	required,
 }: TextAreaType) {
+	const { register } = useFormContext();
 	return (
 		<div className="sm:col-span-2">
 			<InputLabel text={"Your message"} htmlFor={id} />
@@ -16,6 +18,7 @@ export default function TextArea({
 				className="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
 				placeholder={placeholder}
 				required={required}
+				{...register(id)}
 			></textarea>
 		</div>
 	);
