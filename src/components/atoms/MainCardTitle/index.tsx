859a2058a270d4857cc00bd8063255821a81a162
badkamyk <slothmateusz@gmail.com>
2023-02-12 11:32:46 +0100

    refactor: used text prop for MainCardTitle atom

--- src/components/atoms/MainCardTitle/index.tsx
+++ src/components/atoms/MainCardTitle/index.tsx
@@ -1,3 +1,3 @@
 export default function MainCardTitle({ text }: { text: string }) {
-	return <p className="text-lg font-semibold text-slate-800">Hover me!</p>;
+	return <p className="text-lg font-semibold text-slate-800">{text}</p>;
 }
