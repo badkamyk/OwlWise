5495a2a8e997b99ad0eb7cbb899dfc7d16e83e3a
badkamyk <slothmateusz@gmail.com>
2023-02-12 11:33:55 +0100

    style: changed ProcessSection text

--- src/components/organisms/ProcessSection/index.tsx
+++ src/components/organisms/ProcessSection/index.tsx
@@ -8,7 +8,7 @@ export default function ProcessSection() {
 	});
 	return (
 		<div className="flex h-fit flex-col justify-center gap-8 bg-gradient-to-b from-gray-600 via-rose-100 to-teal-100 px-3 py-9 text-center md:px-0">
-			<HeadingMain secondPhrase={"Our services"} />
+			<HeadingMain secondPhrase={"Our process"} />
 			<div className="flex h-fit flex-col justify-center gap-8 px-3 py-9 md:flex-row md:px-0">
 				{mainCardElement}
 			</div>
