219019e61277937cd97b81124f15e1312b4b16c6
badkamyk <slothmateusz@gmail.com>
2023-02-12 11:48:43 +0100

    refactor: changed data on Pricing page

--- src/components/pages/Pricing/index.tsx
+++ src/components/pages/Pricing/index.tsx
@@ -5,45 +5,39 @@ import PricingList from "../../molecules/PricingList";
 const pricingData = [
 	{
 		heading: "Starter",
-		paragraph: "Best option for personal use & for your next project.",
-		price: "$29",
+		paragraph: "Best option for small projects that need a simple website.",
+		price: "$1000",
 		points: [
 			"Individual configuration",
 			"No setup, or hidden fees",
-			"Team size: 1 developer",
+			"Team size: 3 developers",
 			"Premium support: 6 months",
 			"Free updates: 6 months",
-		],
-		buttonText: "Get started",
-		buttonHref: "#",
+		]
 	},
 	{
-		heading: "Starter",
-		paragraph: "Best option for personal use & for your next project.",
-		price: "$29",
+		heading: "Advanced",
+		paragraph: "Best option for projects that need a more complex website or app.",
+		price: "$3000",
 		points: [
 			"Individual configuration",
 			"No setup, or hidden fees",
-			"Team size: 10 developer",
+			"Team size: 5 developer",
 			"Premium support:24 months",
 			"Free updates: 24 months",
-		],
-		buttonText: "Get started",
-		buttonHref: "#",
+		]
 	},
 	{
-		heading: "Starter",
-		paragraph: "Best option for personal use & for your next project.",
-		price: "$29",
+		heading: "Enterprise",
+		paragraph: "Best option for large projects that required specialized use cases or a large team.",
+		price: "individual",
 		points: [
 			"Individual configuration",
 			"No setup, or hidden fees",
 			"Team size: 100+ developer",
 			"Premium support: 36 months",
 			"Free updates: 36 months",
-		],
-		buttonText: "Get started",
-		buttonHref: "#",
+		]
 	},
 ];
 
