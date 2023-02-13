9902efad315cbf5f77ae1ac29f940589fa4a9a67
badkamyk <slothmateusz@gmail.com>
2023-02-13 19:05:23 +0100

    test: MainCardTitle atom snapshot test

---
+++ src/components/atoms/MainCardTitle/MainCardTitle.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { expect, it } from "vitest";
+import MainCardTitle from "./index";
+
+it("MainCardTitle with props", () => {
+	const { container } = render(<MainCardTitle text="Hello World" />);
+	expect(screen.getByText("Hello World")).toBeTruthy();
+	expect(
+		container.getElementsByClassName("text-lg font-semibold text-slate-800"),
+	).toBeTruthy();
+	expect(container).toMatchSnapshot();
+});
