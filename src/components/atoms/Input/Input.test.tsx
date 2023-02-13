c2e0acfcc63054cd319528d066a64e46a4858ab1
badkamyk <slothmateusz@gmail.com>
2023-02-13 14:41:50 +0100

    test: Input atom snapshot test

--- src/components/atoms/Input/Input.test.tsx
+++ src/components/atoms/Input/Input.test.tsx
@@ -1,15 +1,14 @@
-import { it, expect } from "vitest";
 import { render, renderHook, screen } from "@testing-library/react";
-import Input from "./index";
 import { FormProvider, useForm } from "react-hook-form";
-
+import { expect, it } from "vitest";
+import Input from "./index";
 
 it("Input with props", () => {
 	const { result } = renderHook(() => useForm());
 	render(
 		<FormProvider {...result.current}>
 			<Input type="text" id="name" name="name" placeholder="Name" required />
-		</FormProvider>,
+		</FormProvider>
 	);
 	expect(result).toBeTruthy();
 	expect(screen.findByPlaceholderText("Name")).toBeTruthy();
