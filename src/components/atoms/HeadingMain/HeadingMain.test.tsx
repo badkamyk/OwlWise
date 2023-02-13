f2bec59a172d7b37899d64027dc25b5fa2421f5d
badkamyk <slothmateusz@gmail.com>
2023-02-13 18:43:10 +0100

    refactor: added text value check

--- src/components/atoms/HeadingMain/HeadingMain.test.tsx
+++ src/components/atoms/HeadingMain/HeadingMain.test.tsx
@@ -1,11 +1,12 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { expect, it } from "vitest";
 import HeadingMain from "./index";
 
 it("HeadingMain with props", () => {
 	const result = render(
-		<HeadingMain firstPhrase="Hello" secondPhrase="World" />
+		<HeadingMain firstPhrase="Hello" secondPhrase="World" />,
 	);
-	expect(result).toBeTruthy();
+	expect(screen.getByText("Hello")).toBeTruthy();
+	expect(screen.getByText("World")).toBeTruthy();
 	expect(result).toMatchSnapshot();
 });
