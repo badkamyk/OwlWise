c48647ed99061d715a924656779b0c14d929fd48
badkamyk <slothmateusz@gmail.com>
2023-02-14 13:20:59 +0100

    test: ContactHeading molecule snapshot test

---
+++ src/components/molecules/ContactHeading/ContactHeading.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { expect, it } from "vitest";
+import ContactHeading from "./index";
+
+it("ContactHeading", () => {
+	const { container } = render(<ContactHeading />);
+	expect(screen.getByText("Contact")).toBeTruthy();
+	expect(screen.getByText("us")).toBeTruthy();
+	expect(
+		screen.getByText(
+			"Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know."
+		)
+	).toBeTruthy();
+	expect(container).toMatchSnapshot();
+});
