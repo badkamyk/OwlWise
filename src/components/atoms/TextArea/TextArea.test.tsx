5de8fb9c3acb9304853201c0ed06f0326b4fabb6
badkamyk <slothmateusz@gmail.com>
2023-02-14 11:05:56 +0100

    test: TextArea atom snapshot test

---
+++ src/components/atoms/TextArea/TextArea.test.tsx
@@ -0,0 +1,22 @@
+import { render, renderHook, screen } from "@testing-library/react";
+import { FormProvider, useForm } from "react-hook-form";
+import { expect, it } from "vitest";
+import TextArea from "./index";
+
+it("renders TextArea", () => {
+	const { result } = renderHook(() => useForm());
+	const { container } = render(
+		<FormProvider {...result.current}>
+			<TextArea id="message" rows={4} placeholder="Your message" required />
+		</FormProvider>
+	);
+	expect(screen.getByRole("textbox")).toBeTruthy();
+	expect(screen.getByRole("textbox")).toHaveProperty("id", "message");
+	expect(screen.getByRole("textbox")).toHaveProperty(
+		"placeholder",
+		"Your message"
+	);
+	expect(screen.getByRole("textbox")).toHaveProperty("required", true);
+	expect(container.getElementsByClassName("sm:col-span-2")).toBeTruthy();
+	expect(container).toMatchSnapshot();
+});
