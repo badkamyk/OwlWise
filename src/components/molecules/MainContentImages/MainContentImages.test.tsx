5ef55fbe9d0293847eaacccd6d30bd916b3b942d
badkamyk <slothmateusz@gmail.com>
2023-02-14 15:04:13 +0100

    test: MainContentImages molecule snapshot test

---
+++ src/components/molecules/MainContentImages/MainContentImages.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { expect, it } from "vitest";
+import MainContentImages from "./index";
+
+it("MainContentImages", () => {
+	const { container } = render(
+		<MainContentImages
+			firstSrc={"https://via.placeholder.com/150"}
+			firstAlt={"firstAlt"}
+			secondSrc={"https://via.placeholder.com/150"}
+			secondAlt={"secondAlt"}
+			firstClass={"firstClass"}
+			secondClass={"secondClass"}
+		/>
+	);
+	const allImages = screen.getAllByRole("img");
+	expect(
+		container.getElementsByClassName("mt-8 grid grid-cols-2 gap-4")
+	).toBeTruthy();
+	expect(allImages).toHaveLength(2);
+	allImages.forEach((image, i) => {
+		expect(image).toHaveProperty("src", "https://via.placeholder.com/150");
+		expect(image).toHaveProperty("alt", i === 0 ? "firstAlt" : "secondAlt");
+	});
+	expect(container).toMatchSnapshot();
+});
