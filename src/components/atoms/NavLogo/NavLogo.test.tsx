9fd9aaf9f37ef056337f47934c665ff081cc799f
badkamyk <slothmateusz@gmail.com>
2023-02-14 11:04:07 +0100

    test: NavLogo atom snapshot test

---
+++ src/components/atoms/NavLogo/NavLogo.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { expect, it } from "vitest";
+import NavLogo from "./index";
+
+it("NavLogo with props", () => {
+	const { container } = render(
+		<NavLogo href="/" src="/favicon.ico" alt="Logo" text="Logo" />
+	);
+	expect(container.getElementsByClassName("flex items-center")).toBeTruthy();
+	expect(container.getElementsByClassName("mr-3 h-6 sm:h-9")).toBeTruthy();
+	expect(screen.getByRole("link")).toHaveProperty("href", "/");
+	expect(
+		container.getElementsByClassName(
+			"self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+		)
+	).toBeTruthy();
+	expect(screen.getByText("Logo")).toBeTruthy();
+	expect(container).toMatchSnapshot();
+});
