f2905a4dfef204f2681c031dcf600a51b2a672d1
badkamyk <slothmateusz@gmail.com>
2023-02-14 14:41:03 +0100

    test: MainCardContent molecule snapshot test

---
+++ src/components/molecules/MainCardContent/MainCardContent.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import "intersection-observer";
+import { expect, it } from "vitest";
+import MainCardContent from "./index";
+
+it("MainCardContent", () => {
+	const { container } = render(
+		<MainCardContent
+			title={"title"}
+			description={"description"}
+			src={"https://via.placeholder.com/150"}
+		/>
+	);
+	expect(
+		container.getElementsByClassName("relative rounded-[15px] bg-white p-6")
+	).toBeTruthy();
+	expect(screen.getByText("title")).toBeTruthy();
+	expect(screen.getByText("description")).toBeTruthy();
+	expect(screen.getByRole("img")).toBeTruthy();
+	expect(screen.getByRole("img")).toHaveProperty(
+		"src",
+		"https://via.placeholder.com/150"
+	);
+	expect(container).toMatchSnapshot();
+});
