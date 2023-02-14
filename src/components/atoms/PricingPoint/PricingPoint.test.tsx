3fa0ba14177b230c3c85268a38bc7bf6d669bf78
badkamyk <slothmateusz@gmail.com>
2023-02-14 11:03:33 +0100

    test: PricingPoint atom snapshot test

---
+++ src/components/atoms/PricingPoint/PricingPoint.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { expect, it } from "vitest";
+import PricingPoint from "./index";
+
+it("PricingPoint with props", () => {
+	const { container } = render(<PricingPoint text="PricingPoint" />);
+	expect(
+		container.getElementsByClassName("flex items-center space-x-3")
+	).toBeTruthy();
+
+	expect(
+		container.getElementsByClassName(
+			"h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
+		)
+	).toBeTruthy();
+
+	expect(screen.getByText("PricingPoint")).toBeTruthy();
+	expect(container).toMatchSnapshot();
+});
