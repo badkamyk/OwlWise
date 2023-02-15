7f0fac49f11cc9548dd50080967b5cbdda0064df
badkamyk <slothmateusz@gmail.com>
2023-02-15 10:54:05 +0100

    test: MainContentText molecule snapshot test

---
+++ src/components/molecules/MainContentText/MainContentText.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { expect, it } from "vitest";
+import MainContentText from "./index";
+
+it("MainContentText", () => {
+	const { container } = render(
+		<MainContentText
+			headingText={"headingText"}
+			paragraphText={"paragraphText"}
+			secondaryText={"secondaryText"}
+		/>
+	);
+	expect(
+		container.getElementsByClassName(
+			"font-light text-gray-500 dark:text-gray-400 sm:text-lg"
+		)
+	).toBeTruthy();
+	expect(screen.getByText("headingText")).toBeTruthy();
+	expect(screen.getByText("paragraphText")).toBeTruthy();
+	expect(screen.getByText("secondaryText")).toBeTruthy();
+	expect(container.getElementsByClassName("text-xl")).toBeTruthy();
+	expect(container).toMatchSnapshot();
+});
