9fd0b33344c9f7ddaa6f8f8d5169c894761a2180
badkamyk <slothmateusz@gmail.com>
2023-02-17 21:31:46 +0100

    refactor: added onSubmit prop, added FormModal after submitting form, moved formSchema and onSubmit function to separate utils directory

--- src/components/molecules/ContactForm/index.tsx
+++ src/components/molecules/ContactForm/index.tsx
@@ -1,107 +1,99 @@
-import emailjs from "@emailjs/browser";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRef } from "react";
-import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
-import { z } from "zod";
+import { useRef, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { formSchema, FormSchemaType } from "../../../utils/form/formSchema";
+import { onSubmitType } from "../../../utils/form/onSubmitForm";
+import FormModal from "../../atoms/FormModal";
 import HeadingButton from "../../atoms/HeadingButton";
 import TextArea from "../../atoms/TextArea";
 import CompleteInput from "../CompleteInput";
 
-const formSchema = z.object({
-	email: z.string().email("Email must be valid"),
-	subject: z.string().min(3, "Subject must be at least 3 characters"),
-	message: z.string().min(10, "Message must be at least 10 characters"),
-});
-
-type FormSchemaType = z.infer<typeof formSchema>;
-
-export default function ContactForm() {
+export default function ContactForm({
+	onSubmit,
+}: {
+	onSubmit: (data: onSubmitType) => void;
+}) {
 	const methods = useForm<FormSchemaType>({
 		resolver: zodResolver(formSchema),
 	});
 	const formRef = useRef<HTMLFormElement>(null);
-	const onSubmit: SubmitHandler<FormSchemaType> = () => {
-		emailjs
-			.sendForm(
-				"YOUR_SERVICE_ID",
-				"YOUR_TEMPLATE_ID",
-				formRef.current as string | HTMLFormElement,
-				"YOUR_PUBLIC_KEY"
-			)
-			.then(
-				result => {
-					console.log(result.text);
-				},
-				error => {
-					console.log(error.text);
-				}
-			);
-		methods.reset();
-	};
+	const [isModal, setIsModal] = useState(false);
 
 	return (
-		<FormProvider {...methods}>
-			<form
-				ref={formRef}
-				className="space-y-8"
-				onSubmit={methods.handleSubmit(onSubmit)}
-			>
-				<CompleteInput
-					type={"email"}
-					id={"email"}
-					placeholder={"youremail@example.com"}
-					text={"Your email"}
-					required={true}
-				/>
-				{methods.formState.errors.email && (
-					<>
-						<span className="mt-2 block text-red-600">
-							{methods.formState.errors.email.message}
-						</span>
-						<span role="alert" className="sr-only">
-							{methods.formState.errors.email.message}
-						</span>
-					</>
-				)}
+		<>
+			<FormProvider {...methods}>
+				<form
+					data-testid="contact-form"
+					ref={formRef}
+					className="space-y-8"
+					onSubmit={methods.handleSubmit(() =>
+						onSubmit({ formRef, methods, setIsModal })
+					)}
+				>
+					<CompleteInput
+						type={"email"}
+						id={"email"}
+						placeholder={"youremail@example.com"}
+						text={"Your email"}
+						required={true}
+					/>
+					{methods.formState.errors.email && (
+						<>
+							<span className="mt-2 block text-red-600">
+								{methods.formState.errors.email.message}
+							</span>
+							<span role="alert" className="sr-only">
+								{methods.formState.errors.email.message}
+							</span>
+						</>
+					)}
 
-				<CompleteInput
-					type={"text"}
-					id={"subject"}
-					placeholder={"Your subject"}
-					text={"Subject"}
-					required={true}
+					<CompleteInput
+						type={"text"}
+						id={"subject"}
+						placeholder={"Your subject"}
+						text={"Subject"}
+						required={true}
+					/>
+					{methods.formState.errors.subject && (
+						<>
+							<span className="mt-2 block text-red-600">
+								{methods.formState.errors.subject.message}
+							</span>
+							<span role="alert" className="sr-only">
+								{methods.formState.errors.subject.message}
+							</span>
+						</>
+					)}
+					<TextArea
+						id={"message"}
+						placeholder={"Your message..."}
+						required={true}
+					/>
+					{methods.formState.errors.message && (
+						<>
+							<span className="mt-2 block text-red-600">
+								{methods.formState.errors.message.message}
+							</span>
+							<span role="alert" className="sr-only">
+								{methods.formState.errors.message.message}
+							</span>
+						</>
+					)}
+					<HeadingButton
+						text={"Send message"}
+						type={"submit"}
+						disabled={methods.formState.isSubmitting}
+					/>
+				</form>
+				)
+			</FormProvider>
+			{isModal && (
+				<FormModal
+					setIsModal={setIsModal}
+					text={"Your message has been sent."}
 				/>
-				{methods.formState.errors.subject && (
-					<>
-						<span className="mt-2 block text-red-600">
-							{methods.formState.errors.subject.message}
-						</span>
-						<span role="alert" className="sr-only">
-							{methods.formState.errors.subject.message}
-						</span>
-					</>
-				)}
-				<TextArea
-					id={"message"}
-					placeholder={"Your message..."}
-					required={true}
-				/>
-				{methods.formState.errors.message && (
-					<>
-						<span className="mt-2 block text-red-600">
-							{methods.formState.errors.message.message}
-						</span>
-						<span role="alert" className="sr-only">
-							{methods.formState.errors.message.message}
-						</span>
-					</>
-				)}
-				<HeadingButton
-					text={"Send message"}
-					type={"submit"}
-					disabled={methods.formState.isSubmitting}
-				/>
-			</form>
-		</FormProvider>
+			)}
+		</>
 	);
 }
