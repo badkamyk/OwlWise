e8d995f416ff32d803d0c88f7352ca5b01e0772a
badkamyk <slothmateusz@gmail.com>
2023-02-17 21:33:42 +0100

    test: added test for form submitting and calling onSubmit function

--- src/components/molecules/ContactForm/ContactForm.test.tsx
+++ src/components/molecules/ContactForm/ContactForm.test.tsx
@@ -1,22 +1,55 @@
-import { render, renderHook, screen } from "@testing-library/react";
+import { fireEvent, waitFor } from "@testing-library/dom";
+import { act, render, renderHook, screen } from "@testing-library/react";
 import { FormProvider, useForm } from "react-hook-form";
-import { expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import ContactForm from "./index";
 
-it("renders ContactForm correctly", () => {
-	const { result } = renderHook(() => useForm());
-	const { container } = render(
-		<FormProvider {...result.current}>
-			<ContactForm />
-		</FormProvider>
-	);
-	expect(screen.getAllByRole("textbox")).toHaveLength(3);
-	expect(screen.getByPlaceholderText("youremail@example.com")).toBeTruthy();
-	expect(screen.getByText("Your email")).toBeTruthy();
-	expect(screen.getByPlaceholderText("Your subject")).toBeTruthy();
-	expect(screen.getByText("Subject")).toBeTruthy();
-	expect(screen.getByPlaceholderText("Your message...")).toBeTruthy();
-	expect(screen.getByText("Your message")).toBeTruthy();
-	expect(screen.getByRole("button")).toHaveProperty("type", "submit");
-	expect(container).toMatchSnapshot();
+describe("ContactForm", () => {
+	const onSubmit = vi.fn();
+	it("renders ContactForm correctly", () => {
+		const { result } = renderHook(() => useForm());
+		const { container } = render(
+			<FormProvider {...result.current}>
+				<ContactForm onSubmit={onSubmit} />
+			</FormProvider>
+		);
+		expect(screen.getAllByRole("textbox")).toHaveLength(3);
+		expect(screen.getByPlaceholderText("youremail@example.com")).toBeTruthy();
+		expect(screen.getByText("Your email")).toBeTruthy();
+		expect(screen.getByPlaceholderText("Your subject")).toBeTruthy();
+		expect(screen.getByText("Subject")).toBeTruthy();
+		expect(screen.getByPlaceholderText("Your message...")).toBeTruthy();
+		expect(screen.getByText("Your message")).toBeTruthy();
+		expect(screen.getByRole("button")).toHaveProperty("type", "submit");
+		expect(container).toMatchSnapshot();
+	});
+
+	it("Calls onSubmit after submitting the form", async () => {
+		const { result } = renderHook(() => useForm());
+		render(
+			<FormProvider {...result.current}>
+				<ContactForm onSubmit={onSubmit} />
+			</FormProvider>
+		);
+		fireEvent.input(screen.getByPlaceholderText("youremail@example.com"), {
+			target: {
+				value: "email@example.com",
+			},
+		});
+		fireEvent.input(screen.getByPlaceholderText("Your subject"), {
+			target: {
+				value: "subject",
+			},
+		});
+		fireEvent.input(screen.getByPlaceholderText("Your message..."), {
+			target: {
+				value: "this is a test message from user",
+			},
+		});
+		await act(() => {
+			fireEvent.submit(screen.getByTestId("contact-form"));
+		});
+
+		await waitFor(() => expect(onSubmit).toHaveBeenCalled());
+	});
 });
