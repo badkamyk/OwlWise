53f5441d051569c3fad63ccc7c55b82d24d53b9b
badkamyk <slothmateusz@gmail.com>
2023-02-18 21:27:26 +0100

    feat: added data-testid

--- src/components/molecules/PricingCardHeading/index.tsx
+++ src/components/molecules/PricingCardHeading/index.tsx
@@ -8,13 +8,13 @@ export default function PricingCardHeading({
 	price,
 }: PricingCardHeadingType) {
 	return (
-		<>
+		<div data-testid="pricing-card-heading">
 			<ContentHeading text={heading} size={"xl"} color={"white"} />
 			<MainContentParagraph color={"white"} text={paragraph} />
 			<div className="my-8 flex items-baseline justify-center">
 				<span className="mr-2 text-5xl font-extrabold text-white">{price}</span>
 				<span className="text-gray-500 dark:text-white">/month</span>
 			</div>
-		</>
+		</div>
 	);
 }
--- src/components/molecules/PricingHeader/index.tsx
+++ src/components/molecules/PricingHeader/index.tsx
@@ -3,7 +3,10 @@ import MainContentParagraph from "../../atoms/MainContentParagraph";
 
 export default function PricingHeader() {
 	return (
-		<div className="mx-auto mb-8 max-w-screen-md text-center lg:mb-12">
+		<div
+			className="mx-auto mb-8 max-w-screen-md text-center lg:mb-12"
+			data-testid="pricing-header"
+		>
 			<ContentHeading
 				text={"Designed for business teams like yours"}
 				size={"4xl"}
