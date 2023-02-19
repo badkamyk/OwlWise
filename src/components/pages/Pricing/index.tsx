49e682cd24cc9339a95f43c08eeaee9ad88dd39f
badkamyk <slothmateusz@gmail.com>
2023-02-19 14:09:13 +0100

    refactor: moved pricingData to separate file

--- src/components/pages/Pricing/index.tsx
+++ src/components/pages/Pricing/index.tsx
@@ -1,47 +1,7 @@
+import { pricingData } from "../../../utils/pricingData";
 import PricingCardHeading from "../../molecules/PricingCardHeading";
 import PricingHeader from "../../molecules/PricingHeader";
 import PricingList from "../../molecules/PricingList";
-
-const pricingData = [
-	{
-		heading: "Starter",
-		paragraph: "Best option for small projects that need a simple website.",
-		price: "$1000",
-		points: [
-			"Individual configuration",
-			"No setup, or hidden fees",
-			"Team size: 3 developers",
-			"Premium support: 6 months",
-			"Free updates: 6 months",
-		],
-	},
-	{
-		heading: "Advanced",
-		paragraph:
-			"Best option for projects that need a more complex website or app.",
-		price: "$3000",
-		points: [
-			"Individual configuration",
-			"No setup, or hidden fees",
-			"Team size: 5 developer",
-			"Premium support:24 months",
-			"Free updates: 24 months",
-		],
-	},
-	{
-		heading: "Enterprise",
-		paragraph:
-			"Best option for large projects that required specialized use cases or a large team.",
-		price: "individual",
-		points: [
-			"Individual configuration",
-			"No setup, or hidden fees",
-			"Team size: 100+ developer",
-			"Premium support: 36 months",
-			"Free updates: 36 months",
-		],
-	},
-];
 
 export default function Pricing() {
 	const pricingCards = pricingData.map(card => (
---
+++ src/utils/pricingData.tsx
@@ -0,0 +1,40 @@
+export const pricingData = [
+	{
+		heading: "Starter",
+		paragraph: "Best option for small projects that need a simple website.",
+		price: "$1000",
+		points: [
+			"Individual configuration",
+			"No setup, or hidden fees",
+			"Team size: 3 developers",
+			"Premium support: 6 months",
+			"Free updates: 6 months",
+		],
+	},
+	{
+		heading: "Advanced",
+		paragraph:
+			"Best option for projects that need a more complex website or app.",
+		price: "$3000",
+		points: [
+			"Individual configuration",
+			"No setup, or hidden fees",
+			"Team size: 5 developer",
+			"Premium support:24 months",
+			"Free updates: 24 months",
+		],
+	},
+	{
+		heading: "Enterprise",
+		paragraph:
+			"Best option for large projects that required specialized use cases or a large team.",
+		price: "individual",
+		points: [
+			"Individual configuration",
+			"No setup, or hidden fees",
+			"Team size: 100+ developer",
+			"Premium support: 36 months",
+			"Free updates: 36 months",
+		],
+	},
+];
